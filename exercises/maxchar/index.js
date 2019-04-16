c8a9f60e2b4e7aa147ae8008aa947c810065890a
Kyle Davies <kdavies311@gmail.com>
2019-04-16 15:29:08 -0700

    completed maxChar toy problem

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,21 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let storage = {};
+  let char = "";
+  let count = 0;
+
+  for ( var letter of str ) {
+    storage[letter]? storage[letter] ++ : storage[letter] = 1;
+  }
+  for ( var key in storage ) {
+    if ( storage[key] > count ) {
+      char = key;
+      count = storage[key];
+    }
+  }
+  return char;
+}
 
 module.exports = maxChar;
